4ac801d9c8b1e4902ee3d54f35496c20f686dc83
VITianLalit <98540540+VITianLalit@users.noreply.github.com>
2024-03-14 15:52:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_animation_animated_list_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_animation_animated_list_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_animated_list_widget/page/members.dart';
+
+void main() => runApp(MaterialApp(
+  home: MemberPage(),
+  debugShowCheckedModeBanner: false,
+));
